5c7ad7f87fd6333e800bc85b226338c7416e1610
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-11-25 18:01:01 +0530

    onboarding flow slightly modified

---
+++ src/app/actions/onBoardingCheck.js
@@ -0,0 +1,14 @@
+'use server'
+
+import { prisma } from '@/lib/prisma'
+export async function onBoardingCheck(userId) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    })
+    return user.role;
+  } catch (error) {
+    console.error('Failed to update user type:', error)
+    return { success: false, error: 'Failed to update user type' }
+  }
+}
--- src/app/onboarding/layout.jsx
+++ src/app/onboarding/layout.jsx
@@ -2,13 +2,16 @@ import { getServerSession } from 'next-auth/next'
 import { redirect } from 'next/navigation'
 import OnboardingPage from './page'
 import { authOptions } from '@/lib/auth'
+import { onBoardingCheck } from '../actions/onBoardingCheck'
 
 export default async function OnboardingLayout() {
   const session = await getServerSession(authOptions)
-
   if (!session || !session.user) {
     redirect('/sign-in')
   }
+
+  const onboaringStatus = await onBoardingCheck(session?.user.id);
+  if(onboaringStatus !== 'NOT_SET') redirect('/')
 
   return <OnboardingPage userId={session.user.id} />
 }
