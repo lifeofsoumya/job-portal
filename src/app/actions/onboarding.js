236cdc170bb2a984d8fc6454104f088f40550d8c
lifeofsoumya <mondalsoumya2001@gmail.com>
2024-12-13 17:38:46 +0530

    onboarding check added

--- src/app/actions/onboarding.js
+++ src/app/actions/onboarding.js
@@ -16,3 +16,16 @@ export async function updateUserType(userId, userType) {
     return { success: false, error: 'Failed to update user type' }
   }
 }
+
+export async function onBoardingCheck(userId) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    })
+    if(!user) return null;
+    return user.role;
+  } catch (error) {
+    console.error('Failed to update user type:', error)
+    return { success: false, error: 'Failed to update user type' }
+  }
+}
